c23cc9a54904b052c62b4d9871030b9cfdbd1319
MightyPhantom <herewegojes1@gmail.com>
2017-04-21 17:48:06 -0400

    Roles now record correctly.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,6 +2,6 @@ class RegistrationsController < Devise::RegistrationsController
    before_filter :update_sanitized_params
 
    def update_sanitized_params
-       devise_parameter_sanitizer.permit(:sign_up, keys: [:login, :email, :password, :password_confirmation])    
+       devise_parameter_sanitizer.permit(:sign_up, keys: [:login, :email, :password, :password_confirmation, :role])    
     end
 end
