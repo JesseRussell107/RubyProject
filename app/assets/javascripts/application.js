9019f147a5d67e43736ae8bbe84654ca794bbc52
MightyPhantom <herewegojes1@gmail.com>
2017-04-21 16:08:28 -0400

    Finally got it working

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//
 //= require jquery
-//= require jquery.turbolinks
 //= require jquery-ui
 //= require jquery_ujs
-//= require turbolinks
 //= require_tree .
