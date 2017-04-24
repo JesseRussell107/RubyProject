584e1dc30785d47268a18183a88df904023f87b3
MightyPhantom <herewegojes1@gmail.com>
2017-04-23 21:57:08 -0400

    Have the startyear thing working now.

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -71,7 +71,6 @@ class PlansController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def plan_params
-      params.require(:plan).permit(:name)
-      params.require(:plan).permit(:startyear)
+      params.require(:plan).permit(:startyear, :name)
     end
 end
