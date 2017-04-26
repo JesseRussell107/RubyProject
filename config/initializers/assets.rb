0ead24c7420d026437a0d76207071ce49f7e9c7f
Richard Lively <richielively@gmail.com>
2017-04-26 13:19:35 -0400

    Use a manfiest css file instead for the show page

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,5 +10,4 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
-Rails.application.config.assets.precompile += %w( show/Project3.css )
-Rails.application.config.assets.precompile += %w( show/jquery.dataTables.css )
+Rails.application.config.assets.precompile += %w( show/show.css )
